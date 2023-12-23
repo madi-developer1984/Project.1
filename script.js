ab0903d313050bf5571600cbe674f89cd87a9b5e
madi-developer1984 <madi84qi@gmail.com>
2023-12-23 12:00:08 +0600

    Добавлены index.html script.js в ветку lesson01

---
+++ script.js
@@ -0,0 +1,19 @@
+
+var title = "Название проекта";
+var screens = "Простые, Сложные, Интерактивные";
+var screenPrice = 1500;
+var rollback = 20;
+var fullPrice = 50000;
+var adaptive = true;
+
+
+alert("Это модальное окно. Здесь можно вывести любой текст.");
+console.log("Это сообщение в консоли. Здесь тоже можно вывести любой текст.");
+
+
+console.log("Название проекта:", title);
+console.log("Категории экранов:", screens);
+console.log("Цена одного экрана:", screenPrice);
+console.log("Откат:", rollback);
+console.log("Общая стоимость:", fullPrice);
+console.log("Адаптивность:", adaptive);
