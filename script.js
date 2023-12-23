ac160516d8fb6d0f6d0a33bae2e3d5f325a35696
madi-developer1984 <madi84qi@gmail.com>
2023-12-23 12:32:05 +0600

    Добавлены переменные и выполнены задачи

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 
-var title = "Название проекта";
+var title = "Frontend";
 var screens = "Простые, Сложные, Интерактивные";
-var screenPrice = 1500;
-var rollback = 20;
-var fullPrice = 50000;
+var screenPrice = 1500;  
+var rollback = 20;  
+var fullPrice = 50000;  
 var adaptive = true;
 
 
-alert("Это модальное окно. Здесь можно вывести любой текст.");
-console.log("Это сообщение в консоли. Здесь тоже можно вывести любой текст.");
+console.log("Тип данных переменной title:", typeof title);
+console.log("Тип данных переменной fullPrice:", typeof fullPrice);
+console.log("Тип данных переменной adaptive:", typeof adaptive);
 
+console.log("Длина строки из переменной screens:", screens.length);
 
-console.log("Название проекта:", title);
-console.log("Категории экранов:", screens);
-console.log("Цена одного экрана:", screenPrice);
-console.log("Откат:", rollback);
-console.log("Общая стоимость:", fullPrice);
-console.log("Адаптивность:", adaptive);
+var currency = "тенге";
+
+console.log(`Стоимость верстки экранов (${screenPrice}) ${currency}`);
+console.log(`Стоимость разработки сайта (${fullPrice}) ${currency}`);
+
+var screensArray = screens.toLowerCase().split(', ');
+console.log("Массив экранов:", screensArray);
+
+console.log("Процент отката посреднику за работу:", fullPrice * (rollback / 100));
+
+console.log("Все проверено и работает!");
