480a0785b35ed375f80b96776f24042796823dd4
madi-developer1984 <madi84qi@gmail.com>
2023-12-23 15:21:39 +0600

    Добавление функции и выполнены задачи

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-var title = prompt("Как называется ваш проект?");
-var screens = prompt("Какие типы экранов нужно разработать? (Пример: Простые, Сложные, Интерактивные)");
-var screenPrice = parseFloat(prompt("Сколько будет стоить данная работа? (Пример: 12000)"));
-var adaptive = confirm("Нужен ли адаптив на сайте?");
+var getAllServicePrices = function () {
+    return servicePrice1 + servicePrice2;
+};
 
-var service1 = prompt("Какой дополнительный тип услуги нужен?");
-var servicePrice1 = parseFloat(prompt(`Сколько стоит ${service1}?`));
-var service2 = prompt("Какой еще дополнительный тип услуги нужен?");
-var servicePrice2 = parseFloat(prompt(`Сколько стоит ${service2}?`));
-
-var fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-var servicePercentPrice = Math.ceil(fullPrice * (1 - rollback / 100));
-
-if (fullPrice > 30000) {
-    console.log("Даем скидку в 10%");
-} else if (fullPrice > 15000 && fullPrice <= 30000) {
-    console.log("Даем скидку в 5%");
-} else if (fullPrice > 0 && fullPrice <= 15000) {
-    console.log("Скидка не предусмотрена");
-} else {
-    console.log("Что-то пошло не так");
+function getFullPrice() {
+    return screenPrice + getAllServicePrices();
 }
+var fullPrice = getFullPrice();  
+
+function getTitle() {
+    return title.replace(/\b\w/g, (match) => match.toUpperCase());
+}
+var formattedTitle = getTitle(); 
+
+function getServicePercentPrices() {
+    return Math.ceil(fullPrice * (1 - rollback / 100));
+}
+var servicePercentPrice = getServicePercentPrices();
+
+console.clear(); 
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+console.log("Типы экранов для разработки:", screens);
+
+getRollbackMessage();
+
+console.log("Стоимость за вычетом процента отката посреднику:", servicePercentPrice);
 
 console.log("Все проверено и работает!");
