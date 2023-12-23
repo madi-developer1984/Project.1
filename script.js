459359eaa7818ff70a66d42fcef95b70003758f0
madi-developer1984 <madi84qi@gmail.com>
2023-12-23 15:43:52 +0600

    Добавлены фнкции и выполнены задачи

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var title = prompt("Как называется ваш проект?");
+var screens = prompt("Какие типы экранов нужно разработать? (Пример: Простые, Сложные, Интерактивные)");
+var screenPrice = parseFloat(prompt("Сколько будет стоить данная работа? (Пример: 12000)"));
+var rollback = parseFloat(prompt("Какой процент отката посреднику предусмотрен? (Пример: 10)"));
+var adaptive = confirm("Нужен ли адаптив на сайте?");
+
+var service1 = prompt("Какой дополнительный тип услуги нужен?");
+var servicePrice1 = parseFloat(prompt("Сколько будет стоить " + service1 + "?"));
+var service2 = prompt("Какой еще дополнительный тип услуги нужен?");
+var servicePrice2 = parseFloat(prompt("Сколько будет стоить " + service2 + "?"));
+
 var getAllServicePrices = function () {
     return servicePrice1 + servicePrice2;
 };
@@ -5,28 +16,17 @@ var getAllServicePrices = function () {
 function getFullPrice() {
     return screenPrice + getAllServicePrices();
 }
-var fullPrice = getFullPrice();  
-
-function getTitle() {
-    return title.replace(/\b\w/g, (match) => match.toUpperCase());
-}
-var formattedTitle = getTitle(); 
-
-function getServicePercentPrices() {
-    return Math.ceil(fullPrice * (1 - rollback / 100));
-}
-var servicePercentPrice = getServicePercentPrices();
+var fullPrice = getFullPrice(); 
 
 console.clear(); 
 
-showTypeOf(title);
-showTypeOf(fullPrice);
-showTypeOf(adaptive);
-
-console.log("Типы экранов для разработки:", screens);
-
-getRollbackMessage();
-
-console.log("Стоимость за вычетом процента отката посреднику:", servicePercentPrice);
-
-console.log("Все проверено и работает!");
+console.log("title:", title);
+console.log("screens:", screens);
+console.log("screenPrice:", screenPrice);
+console.log("rollback:", rollback);
+console.log("adaptive:", adaptive);
+console.log("service1:", service1);
+console.log("servicePrice1:", servicePrice1);
+console.log("service2:", service2);
+console.log("servicePrice2:", servicePrice2);
+console.log("fullPrice:", fullPrice);
