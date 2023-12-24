307e1780952e544d4622d636f1db04a131c07361
madi-developer1984 <madi84qi@gmail.com>
2023-12-25 01:21:06 +0600

    Добавлены все функции и выполнены задачи

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var title = prompt("Как называется ваш проект?");
-var screens = prompt("Какие типы экранов нужно разработать? (Пример: Простые, Сложные, Интерактивные)");
-var screenPrice = parseFloat(prompt("Сколько будет стоить данная работа? (Пример: 12000)"));
-var adaptive = confirm("Нужен ли адаптив на сайте?");
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать? (Пример: Простые, Сложные, Интерактивные)");
+let screenPrice = parseFloat(prompt("Сколько будет стоить данная работа? (Пример: 12000)"));
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 
-var service1 = prompt("Какой дополнительный тип услуги нужен?");
-var servicePrice1 = parseFloat(prompt(`Сколько стоит ${service1}?`));
-var service2 = prompt("Какой еще дополнительный тип услуги нужен?");
-var servicePrice2 = parseFloat(prompt(`Сколько стоит ${service2}?`));
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = parseFloat(prompt(`Сколько стоит ${service1}?`));
+let service2 = prompt("Какой еще дополнительный тип услуги нужен?");
+let servicePrice2 = parseFloat(prompt(`Сколько стоит ${service2}?`));
 
-var fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
-var servicePercentPrice = Math.ceil(fullPrice * (1 - rollback / 100));
+let servicePercentPrice = Math.ceil(fullPrice * (1 - rollback / 100));
 
 if (fullPrice > 30000) {
     console.log("Даем скидку в 10%");
