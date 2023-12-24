16aa46bbca529660cb92eef23fd7334cc2db9577
madi-developer1984 <madi84qi@gmail.com>
2023-12-25 01:42:14 +0600

    Добавлены все функции и выполнены задачи

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
-var title = prompt("Как называется ваш проект?");
-var screens = prompt("Какие типы экранов нужно разработать? (Пример: Простые, Сложные, Интерактивные)");
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать? (Пример: Простые, Сложные, Интерактивные)");
 
+let screenPrice;
 do {
-    var screenPriceInput = prompt("Сколько будет стоить данная работа? (Пример: 12000)");
+    let screenPriceInput = prompt("Сколько будет стоить данная работа? (Пример: 12000)");
     screenPrice = parseFloat(screenPriceInput);
 } while (isNaN(screenPrice));
 
-var rollbackInput = prompt("Какой процент отката посреднику предусмотрен? (Пример: 10)");
+let rollbackInput = prompt("Какой процент отката посреднику предусмотрен? (Пример: 10)");
 
 while (isNaN(rollbackInput)) {
     rollbackInput = prompt("Введите корректное значение процента отката (число от 1 до 100):");
 }
-var rollback = parseFloat(rollbackInput);
+let rollback = parseFloat(rollbackInput);
 
-var adaptive = confirm("Нужен ли адаптив на сайте?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 
-var service1 = prompt("Какой дополнительный тип услуги нужен?");
-var servicePrice1Input = prompt("Сколько будет стоить " + service1 + "?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1Input = prompt("Сколько будет стоить " + service1 + "?");
 
 while (isNaN(servicePrice1Input)) {
     servicePrice1Input = prompt("Введите корректное значение стоимости услуги " + service1 + ":");
 }
-var servicePrice1 = parseFloat(servicePrice1Input);
+let servicePrice1 = parseFloat(servicePrice1Input);
 
-var service2 = prompt("Какой еще дополнительный тип услуги нужен?");
-var servicePrice2Input = prompt("Сколько будет стоить " + service2 + "?");
+let service2 = prompt("Какой еще дополнительный тип услуги нужен?");
+let servicePrice2Input = prompt("Сколько будет стоить " + service2 + "?");
 
 while (isNaN(servicePrice2Input)) {
     servicePrice2Input = prompt("Введите корректное значение стоимости услуги " + service2 + ":");
 }
-var servicePrice2 = parseFloat(servicePrice2Input);
-
-var screenPrice = parseFloat(screenPriceInput);
-var rollback = parseFloat(rollbackInput);
-var servicePrice1 = parseFloat(servicePrice1Input);
-var servicePrice2 = parseFloat(servicePrice2Input);
+let servicePrice2 = parseFloat(servicePrice2Input);
 
 console.clear();
 
